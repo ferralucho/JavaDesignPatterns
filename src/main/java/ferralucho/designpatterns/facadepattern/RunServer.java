73faa02a8d3105dad10f6f493bef238845bc2189
Luciano Ferrari <lucianodarioferrari@gmail.com>
2019-03-31 23:47:26 -0300

    Add facade pattern

---
+++ src/main/java/ferralucho/designpatterns/facadepattern/RunServer.java
@@ -0,0 +1,14 @@
+package ferralucho.designpatterns.facadepattern;
+
+public class RunServer {
+    public static void main(String[] args) {
+        ScheduleServer scheduleServer = new ScheduleServer();
+        ScheduleServerFacade facadeServer = new ScheduleServerFacade(scheduleServer);
+        facadeServer.startServer();
+
+        System.out.println("Start working......");
+        System.out.println("After work done.........");
+
+        facadeServer.stopServer();
+    }
+}
