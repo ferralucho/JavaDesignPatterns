c2f5a597582b2f7a21b7f750e5ac8e4ade447f22
Luciano Ferrari <lucianodarioferrari@gmail.com>
2019-03-31 21:50:05 -0300

    Add adapter pattern

---
+++ src/main/java/ferralucho/designpatterns/adapterpattern/payd/PayD.java
@@ -0,0 +1,14 @@
+package ferralucho.designpatterns.adapterpattern.payd;
+
+ public interface PayD {
+     String getCustCardNo();
+     String getCardOwnerName();
+     String getCardExpMonthDate();
+     Integer getCVVNo();
+     Double getTotalAmount();
+     void setCustCardNo(String custCardNo);
+     void setCardOwnerName(String cardOwnerName);
+     void setCardExpMonthDate(String cardExpMonthDate);
+     void setCVVNo(Integer cVVNo);
+     void setTotalAmount(Double totalAmount);
+}
