83f1a4379638fa217f75447edd1c0981a6b200f5
Luciano Ferrari <lucianodarioferrari@gmail.com>
2019-04-02 17:03:32 -0300

    Add prototype pattern

---
+++ src/main/java/ferralucho/designpatterns/prototypepattern/Square.java
@@ -0,0 +1,13 @@
+package ferralucho.designpatterns.prototypepattern;
+
+public class Square extends Shape {
+
+    public Square(){
+        type = "Square";
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Inside Square::draw() method.");
+    }
+}
